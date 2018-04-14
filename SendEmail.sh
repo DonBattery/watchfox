57f1037d8aabf7c1e1ce01aa7644d517058e9490
donbattery <mikloslorinczi.it@gmail.com>
2018-04-14 21:05:08 +0200

    basic html css js added

--- SendEmail.sh
+++ SendEmail.sh
@@ -4,13 +4,21 @@ echo "To:"$1 > allert_email
 echo "Subject: Automatic WatchFox Allert" >> allert_email
 echo "" >> allert_email
 echo "Service "$2 >> allert_email
+
 if [[ $3 != "" ]]; then
   echo $3 >> allert_email
 fi
+
 if [[ $4 != "" ]]; then
   echo $4 >> allert_email
 fi
+
 if [[ $5 != "" ]]; then
   echo $5 >> allert_email
-fi    
+fi   
+
 msmtp -t < allert_email
+
+# Cleanup
+echo "" > allert_email
+rm allert_email
--- servers.ls
+++
@@ -1,4 +0,0 @@
-localhost:1212
-localhost:1213
-index.hu
-localhost:8080
