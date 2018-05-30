56a8fb8576eac0ccb8e2793430e1603cdf9dab5b
donbattery <mikloslorinczi.it@gmail.com>
2018-05-31 00:47:36 +0200

    gitignore test

--- www/data.js
+++ www/data.js
@@ -1,4 +1,4 @@
-const statusData = { timeStamp : "WatchFox report created on 2018/05/30 at 23:56:28", services : [
-{ name : "MyService", url : "localhost:9090", server : "Server OK", app : "APP Error", db : "Database Error"},
+const statusData = { timeStamp : "WatchFox report created on 2018/05/31 at 00:36:17", services : [
+{ name : "MyService", url : "localhost:9090", server : "Server OK", app : "APP OK", db : "Database Error"},
 { name : "Gmail", url : "gmail.com", server : "Server OK", app : "APP OK", db : "Database Error"},
 ]}
