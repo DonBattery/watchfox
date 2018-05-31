37f28a2f37197de13fd32324ab2df5b7074d688e
donbattery <mikloslorinczi.it@gmail.com>
2018-05-31 04:54:28 +0200

    data.js added to .gitignore

--- www/data.js
+++
@@ -1,4 +0,0 @@
-const statusData = { timeStamp : "WatchFox report created on 2018/05/31 at 02:04:02", services : [
-{ name : "MyService", url : "localhost:9090", server : "Server OK", app : "APP Error", db : "Database Error"},
-{ name : "Gmail", url : "gmail.com", server : "Server OK", app : "APP OK", db : "Database Error"},
-]}
